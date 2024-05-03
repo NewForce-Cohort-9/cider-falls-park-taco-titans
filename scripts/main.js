8f807c712a85b915b92dbceda5644d06e1e953e9
Will Moran <will.c.moran@gmail.com>
2024-05-03 13:26:31 -0400

    Altered db, invoked destinations function, altered destination click event, added styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 import { Services } from "./services.js"
 
+import { destinationsHTML } from "./destinations.js"
+
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
+    <section class="destinations">${destinationsHTML()}</section>
     <section class="detail--column list details__services">
         <h2>Services:</h2>
         ${Services()}
